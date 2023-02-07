0592606dc7e1891aeeaa5e0bef9664234dd2f545
Rodrigo <rodrigo@MacBook-Pro-de-Master.local>
2023-02-07 18:53:25 -0300

    project base

---
+++ README.md
@@ -0,0 +1,66 @@
+# 🔗 WebCrawler - Get Site URLs
+
+WebCrawler to get URLs from a given site
+
+## 1. Architecture
+
+### 1.1. Technologies of the project
+
+Belows are the tool used to perform the development of this project.
+- [JavaScript](https://www.javascript.com/)
+- [Express JS](https://expressjs.com/)
+- [Selenium Webdriver](https://www.selenium.dev/)
+
+### 1.2. Prerequisites to run
+
+To use this project, you'll need:
+- Node.js, a Long-Term Support (LTS) release version 16 or later - [download](https://nodejs.org/en/)
+- Google Chrome, latest version - [download](https://www.google.com/intl/en-US/chrome/)
+
+## 2. Usage
+
+### 2.1 Installation / Setup
+
+Once you have the code on your computer, use your computer terminal to run the following command in the directory where you've cloned the project:
+```
+npm install
+```
+
+### 2.1. Command line interface
+
+```shell
+npx get-links-urls your.given-url.com
+```
+
+### 2.2 CLI options / arguments
+
+
+Get URL's from a given website.
+
+Usage
+```
+$ get-links-urls <url>           # Get all URLs from a given website
+$ npx get-links-urls --help		 # Help of the usage of the tool   
+```
+
+Options
+```
+	--output=<string>, -o   File saved to the system
+	--max-depth=<number>,   Maximum depth of routes to search
+```
+
+Examples
+```
+	$ get-site-urls your.given-url.com --output=sitemap.xml
+	✅ Generated sitemap.xml 150 urls found from the tiven website
+```
+
+
+# About myself 
+#### (the guy who received the challenge)
+
+![ralves20-profile-avatar](https://avatars.githubusercontent.com/u/40844089)
+This is my profile on Github: [ralves20](https://github.com/ralves20)
+
+
+
---
+++ index.js
@@ -0,0 +1,107 @@
+import got from "got";
+import normalizeUrl from "normalize-url";
+import { Parser } from "htmlparser2";
+
+export const cleanUrl = (url) =>
+	normalizeUrl(url, {
+		stripHash: true,
+		removeQueryParameters: true,
+	});
+
+export const crawlUrl = async ({
+	url,
+	baseUrl,
+	data,
+	currentDepth,
+	maxDepth,
+	spinner,
+}) => {
+	data.queue.delete(url);
+
+	if (maxDepth === currentDepth) return;
+
+	currentDepth += 1;
+
+	try {
+		const { headers, statusCode } = await got.head(url);
+		if (statusCode !== 200) {
+			console.log(statusCode);
+			data.errors.add(url);
+			return;
+		}
+
+		if (!headers["content-type"].includes("text/html")) {
+			return;
+		}
+
+		data.found.add(url);
+
+		const { body } = await got(url);
+		const parser = new Parser({
+			onopentag(name, attributes) {
+				if (name === "a" && attributes.href) {
+					if (
+						attributes.href.startsWith("#") ||
+						attributes.href.startsWith("mailto:") ||
+						attributes.href.startsWith("tel:") ||
+						attributes.href.startsWith("http")
+					) {
+						return;
+					}
+
+					const newUrl = attributes.href.startsWith("/")
+						? cleanUrl(`${baseUrl}${attributes.href}`)
+						: cleanUrl(`${url}/${attributes.href}`);
+
+					if (!data.found.has(newUrl) && !data.errors.has(newUrl)) {
+						data.queue.add(newUrl);
+					}
+				}
+			},
+		});
+		parser.write(body);
+		parser.end();
+
+		if (data.queue.size > 0) {
+			if (spinner) {
+				spinner.text = `${data.found.size} Found, ${data.queue.size} Queued, ${data.errors.size} Errors`;
+			}
+			const searchSite = [...data.queue].map((url) =>
+				crawlUrl({ url, baseUrl, data, currentDepth, maxDepth, spinner })
+			);
+			await Promise.all(searchSite);
+		}
+	} catch (error) {
+		if (!error.message.includes("404")) {
+			console.error(`Failed to load ${url}:\n${error.message}\n\n`);
+		} else {
+			data.errors.add(`404: ${url}`);
+		}
+	}
+};
+
+const getSiteUrls = async (siteUrl, maxDepth) => {
+	const url = cleanUrl(siteUrl);
+
+	const rawData = {
+		queue: new Set([url]),
+		found: new Set([]),
+		errors: new Set([]),
+	};
+
+	await crawlUrl({
+		url,
+		data: rawData,
+		maxDepth,
+		spinner: null,
+		baseUrl: url,
+		currentDepth: 0,
+	});
+
+	return {
+		found: [...rawData.found].sort(),
+		errors: [...rawData.errors].sort(),
+	};
+};
+
+export default getSiteUrls;
